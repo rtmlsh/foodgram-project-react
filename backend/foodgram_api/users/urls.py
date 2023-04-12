f3e4e04c84e7154d18bc4c3e263c9222de347b19
Артём Малыш <56894987+rtmlsh@users.noreply.github.com>
2023-04-12 20:45:45 +0400

    Добавил подписки

--- backend/foodgram_api/users/urls.py
+++ backend/foodgram_api/users/urls.py
@@ -1,6 +1,14 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CustomUserViewSet
+
+router = DefaultRouter()
+
+router.register('users', CustomUserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/foodgram_api/users/views.py
+++ backend/foodgram_api/users/views.py
@@ -1,10 +1,34 @@
+from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from .serializers import CustomUserSerializer
-from .models import User
-from api.pagination import CustomPagination
+from .models import User, Follow
+from .pagination import CustomPagination
+from rest_framework.response import Response
 
 
 class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     pagination_class = CustomPagination
+
+    @action(methods=['POST', 'DELETE'], detail=True, permission_classes=[IsAuthenticated])
+    def subscribe(self, request, **kwargs):
+        author = get_object_or_404(User, id=self.kwargs.get('id'))
+        subscribe = Follow.objects.filter(user=request.user, following=author)
+
+        if subscribe.exists():
+            if request.method == 'DELETE':
+                subscribe.delete()
+                return Response({'alert': 'Автор убран из подписок'}, status=status.HTTP_204_NO_CONTENT)
+            return Response({'errors': 'Автор уже добавлен в подписки'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if request.method == 'POST':
+            Follow.objects.get_or_create(user=request.user, following=author)
+            return Response({'alert': 'Автор добавлен в подписки'}, status=status.HTTP_201_CREATED)
+
+    @action(methods=['POST', 'DELETE'], detail=True, permission_classes=[IsAuthenticated])
+    def subscriptions(self, request):
+        pass
