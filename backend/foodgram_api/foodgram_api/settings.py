5916446c68b86195158b2cd68feb450272e1c0c2
Артём Малыш <56894987+rtmlsh@users.noreply.github.com>
2023-04-18 23:48:23 +0400

    Отформатировал код

--- backend/foodgram_api/foodgram_api/settings.py
+++ backend/foodgram_api/foodgram_api/settings.py
@@ -79,10 +79,10 @@ DATABASES = {
         'HOST': 'db',
         'PORT': '5432'
     }
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 AUTH_USER_MODEL = 'users.User'
