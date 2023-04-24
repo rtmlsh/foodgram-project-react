91c28d9b55ba2aaf8f92b8228bab35755076031a
Артём Малыш <56894987+rtmlsh@users.noreply.github.com>
2023-04-24 23:53:11 +0400

    Изменил фильтры

--- backend/foodgram_api/api/filters.py
+++ backend/foodgram_api/api/filters.py
@@ -4,29 +4,21 @@ from foodgram.models import Recipe, Tag, User
 
 
 class RecipeFilter(rest_framework.FilterSet):
-    tag = filters.AllValuesFilter(field_name='tags__slug')
+    tags = filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
+        field_name="tags__slug",
+        to_field_name="slug"
+    )
     author = filters.NumberFilter(field_name='author__id')
-    is_favorited = BooleanFilter(method="get_favorite_filter")
-    is_in_shopping_cart = BooleanFilter(method="get_shopping_cart_filter")
+    is_favorited = NumberFilter(method="get_favorite_filter")
+    is_in_shopping_cart = NumberFilter(method="get_shopping_cart_filter")
 
     class Meta:
         model = Recipe
         fields = ("tags", "author",)
 
     def get_favorite_filter(self, queryset, name, value):
-        recipes = Recipe.objects.filter(favorite_recipes__user=self.request.user)
-        return recipes
+        return queryset.filter(favorite_recipes__user=self.request.user)
 
     def get_shopping_cart_filter(self, queryset, name, value):
-        recipes = Recipe.objects.filter(recipes_in_shopping_cart__user=self.request.user)
-        return recipes
-
-
-
-    # def get_favorite_recipe(self, queryset, name, value):
-    #     if value:
-    #         return queryset.filter(favorite_recipes__user=self.request.user)
-    #
-    # def get_shopping_cart(self, queryset, name, value):
-    #     if value:
-    #         return queryset.filter(recipes_in_shopping_cart__user=self.request.user)
+        return queryset.filter(recipes_in_shopping_cart__user=self.request.user)
